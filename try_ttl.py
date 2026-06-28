d7e381e878b884d3fd6730f4c7db0b455931c535
noamashk1 <noam4596>
2026-06-28 10:13:16 +0300

    try ttl

--- try_ttl.py
+++ try_ttl.py
@@ -1,32 +1,33 @@
-# import lgpio
-# import time
+import lgpio
+import time
 
-# GPIO = 18  # BCM numbering
+GPIO17 = 17  # BCM numbering
+GPIO22 = 22  # BCM numbering
+GPIO27 = 27  # BCM numbering
 
-# # Open GPIO chip (0 is correct for Raspberry Pi)
-# h = lgpio.gpiochip_open(0)
+# Open GPIO chip (0 is correct for Raspberry Pi)
+h = lgpio.gpiochip_open(0)
 
-# # Set GPIO18 as output
-# lgpio.gpio_claim_output(h, GPIO)
+# Set GPIO18 as output
+lgpio.gpio_claim_output(h, GPIO17)
+lgpio.gpio_claim_output(h, GPIO22)
+lgpio.gpio_claim_output(h, GPIO27)
+GPIO_lst = [GPIO17,GPIO27, GPIO22]
+idx = 0
+try:
+    while True:
+        GPIO = GPIO_lst[idx%3]
+        lgpio.gpio_write(h, GPIO, 1)  # HIGH (3.3V)
+        print("playing..." + str(GPIO))
+        time.sleep(0.5)               # pulse width
+        lgpio.gpio_write(h, GPIO, 0)  # LOW
+        time.sleep(3)               # rest of 3 sec period
+        idx += 1        
 
-# try:
-#     while True:
-#         lgpio.gpio_write(h, GPIO, 1)  # HIGH (3.3V)
-#         time.sleep(0.5)               # pulse width
-#         lgpio.gpio_write(h, GPIO, 0)  # LOW
-#         time.sleep(2.5)               # rest of 3 sec period
+except KeyboardInterrupt:
+    pass
 
-# except KeyboardInterrupt:
-#     pass
+finally:
+    lgpio.gpio_write(h, GPIO, 0)
+    lgpio.gpiochip_close(h)
 
-# finally:
-#     lgpio.gpio_write(h, GPIO, 0)
-#     lgpio.gpiochip_close(h)
-
-import wave
-
-file_path = "C:\\Users\\noam4\\Downloads\\file_example_WAV_1MG.wav"#"C:\\Users\\noam4\\Downloads\\pup ICR USVs.wav"
-
-with wave.open(file_path, "rb") as wav_file:
-    sampling_rate = wav_file.getframerate()
-    print(f"Sampling Rate: {sampling_rate} Hz")
